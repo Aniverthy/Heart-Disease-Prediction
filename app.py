dfa72fd6d79d946208e5461c33d6e5a8b8bc37d9
Aniverthy <97465130+Aniverthy@users.noreply.github.com>
2022-08-29 20:38:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+
+#load the joblib model
+model_nb=joblib.load('spam-ham')
+
+st.title('HEART DISEASE PREDICTOR')
+
+age=st.number_input('Enter your Age:')
+sex=st.number_input('Enter your Gender:')
+cp=st.number_input('Enter chest pain type:')
+trestbps=st.number_input('Enter resting blood pressure:')
+chol=st.number_input('Enter serum cholestoral in mg/dl:')
+fbs=st.number_input('Enter fasting blood sugar:')
+restecg=st.number_input('Enter resting electrocardiographic results:')
+thalach=st.number_input('Enter maximum heart rate achieved:')
+exang=st.number_input('Enter exercise induced angina:')
+oldpeak=st.number_input('Enter depression induced by exercise relative to rest:')
+ca=st.number_input('Enter number of major vessels (0-3) colored by flourosopy:')
+thal=st.number_input('Enter type of defect:(0 = normal; 1 = fixed defect; 2 = reversable defect):')
+op=model_nb.predict([age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,,ca,thal])
+if st.button('PREDICT'):
+  st.title(op[0])
