6355401e00929623988202e9f94ea58f99801ceb
Aniverthy <97465130+Aniverthy@users.noreply.github.com>
2022-08-29 21:11:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 
 #load the joblib model
-model_nb=joblib.load('spam-ham')
+model_nb=joblib.load('Heart-Disease-Prediction')
 
 st.title('HEART DISEASE PREDICTOR')
 
