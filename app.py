d5e3ca263339153624cb832a0dcdf6643df9954b
Aniverthy <97465130+Aniverthy@users.noreply.github.com>
2022-08-29 21:19:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title('HEART DISEASE PREDICTOR')
 
 age=st.number_input('Enter your Age:')
 sex=st.number_input('Enter your Gender:')
-cp=st.number_input('Enter chest pain type:')
+cp=st.number_input('Enter chest pain type (1=angina, 2=pericarditis, 3=myocarditis:')
 trestbps=st.number_input('Enter resting blood pressure:')
 chol=st.number_input('Enter serum cholestoral in mg/dl:')
 fbs=st.number_input('Enter fasting blood sugar:')
@@ -20,7 +20,10 @@ ca=st.number_input('Enter number of major vessels (0-3) colored by flourosopy:')
 thal=st.number_input('Enter type of defect:(0 = normal; 1 = fixed defect; 2 = reversable defect):')
 op=model_nb.predict([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,ca,thal]])
 if st.button('PREDICT'):
-  st.title(op[0])
+  if st.title(op[0])==1:
+    st.write("no heart disease")
+  else:
+    st.write("you have a heart disease please consult a doctor")
     
     
     
